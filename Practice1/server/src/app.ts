fe474a35fc7fba2fd787cbc607283672c8ceafc4
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 00:57:53 +0600

    Practice1 - server - add: json parser middleware

--- Practice1/server/src/app.ts
+++ Practice1/server/src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import router from './routes/routes';
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use(router);
+app.use('/api', router);
 
 export default app;
