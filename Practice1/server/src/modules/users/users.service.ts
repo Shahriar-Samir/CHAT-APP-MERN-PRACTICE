8080ff381f9ef07441cfa24a9741011a10006354
Shahriar-Samir <shabusiness035@gmail.com>
2024-12-31 12:04:12 +0600

    Practice1 - server - add: users service of delete user from db

--- Practice1/server/src/modules/users/users.service.ts
+++ Practice1/server/src/modules/users/users.service.ts
@@ -5,3 +5,8 @@ export const addUserIntoDB = async (payload: TUser) => {
   const result = await UserModel.create(payload);
   return result;
 };
+
+export const deleteUserFromDB = async (uid: string) => {
+  const result = await UserModel.updateOne({ uid }, { isDeleted: true });
+  return result;
+};
