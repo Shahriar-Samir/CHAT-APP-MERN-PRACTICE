143147e5dbe53f26da1c956155848dfec1d97632
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 15:28:29 +0600

    Practice1/server - remove: unnecessary eslint-disabled-rules

--- Practice1/server/src/app/middlewares/notFound.ts
+++ Practice1/server/src/app/middlewares/notFound.ts
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import status from 'http-status';
 
